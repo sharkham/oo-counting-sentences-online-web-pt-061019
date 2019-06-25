45e448aaaafc1a49a41f0609beafe2266a480646
Sam Markham <sam.e.markham@gmail.com>
2019-06-25 20:14:57 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
+
+  # def count_sentences
+  #   split_sentences = self.split(/[.?!]\s/)
+  #   split_sentences.count
+  # end
 
   def count_sentences
-
+    split_sentences = self.split(/[.?!]/)
+    split_sentences.delete("")
+    split_sentences.count
   end
+
 end
